9fbb8162402a1b3f69fa30778a488f1da40a9a53
RuoFeng207 <ruo2feng@gmail.com>
2026-06-13 22:18:52 +0200

    Main style, css and index html

---
+++ main.js
@@ -0,0 +1,37 @@
+console.log("main loaded");
+
+const BASE = "/";
+
+async function loadComponent(id, file) {
+    const el = document.getElementById(id);
+    if (!el) return;
+
+    try {
+        const url = BASE + "components/" + file;
+
+        const res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error("Failed: " + url);
+        }
+
+        el.innerHTML = await res.text();
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+function loadCSS(file) {
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = BASE + file;
+    document.head.appendChild(link);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadCSS("main.css");
+
+    loadComponent("navbar", "navbar.html");
+    loadComponent("footer", "footer.html");
+});
